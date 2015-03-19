db65a361f452cd3aaed505c573ae09faa3ca5872
Timerkhan <timerkhan-94@mail.ru>
2015-03-19 18:48:03 +0200

    new 1

--- src/ReClass.java
+++ src/ReClass.java
@@ -2,14 +2,14 @@
  * Created by Администратор on 19.03.2015.
  */
 public class ReClass {
-        public static String getnumberstandart(String a) {
+        public static String getNumberStandart(String a) {
             return a.toLowerCase().replaceAll("\\s|\\.", "");
         }
         public static void main(String[] args) throws java.lang.Exception {
             {
                 System.out.println("test for commit to Git");
                 String n = "wasD.12s 2e daDv.sW";
-                System.out.println(getnumberstandart(n));
+                System.out.println(getNumberStandart(n));
             }
         }
 }
