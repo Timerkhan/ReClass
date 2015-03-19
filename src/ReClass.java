d378fb8ddc76a8765ff69ad391341f740e2b3ad9
Timerkhan <timerkhan-94@mail.ru>
2015-03-19 18:27:51 +0200

    Initial commit

---
+++ src/ReClass.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Администратор on 19.03.2015.
+ */
+public class ReClass {
+        public static String getnumberstandart(String a) {
+            return a.toLowerCase().replaceAll("\\s|\\.", "");
+        }
+        public static void main(String[] args) throws java.lang.Exception {
+            {
+                System.out.println("test for commit to Git");
+                String n = "wasD.12s 2e daDv.sW";
+                System.out.println(getnumberstandart(n));
+            }
+        }
+}
