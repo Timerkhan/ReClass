6a358f6939c287a5a99f0daf3364faf1f55d69d8
Timerkhan <timerkhan-94@mail.ru>
2015-03-24 15:09:15 +0200

    Check for null

--- src/ReClass.java
+++ src/ReClass.java
@@ -3,13 +3,16 @@
  */
 public class ReClass {
         public static String getNumberStandart(String a) {
+            if ( a == null ){
+                return "Enter the name";
+            }
+
             return a.toLowerCase().replaceAll("\\s|\\.", "");
         }
         public static void main(String[] args) throws java.lang.Exception {
             {
-                System.out.println("test for commit to Git");
                 String n = "wasD.12s 2e daDv.sW";
-                System.out.println(getNumberStandart(n));
+                System.out.println(getNumberStandart(null));
             }
         }
 }
